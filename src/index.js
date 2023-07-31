25035fd424df5621b0b2fdc51f110c2c63ddb9ff
v-kuntsevich <kwakin@gmail.com>
2023-07-31 13:06:46 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let stringNum = n.toString();
+    let reverseNum = stringNum.replace(/[^0-9]/g, '').split("").reverse().join("");
+    return reverseNum;
 }
